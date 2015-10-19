0ab12f70ec5852367cdf0a01194004333cfdb8d1
One.Zero <sjnilanr@gmail.com>
2015-10-19 12:58:00 +0530

    Create Example.cs

---
+++ Example.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+
+public class Example : MonoBehaviour {
+
+private Texture2D colBack;
+private Texture2D colFront;
+public float Health;
+public float MaxHealth=100f;
+public float HealthBarHeight=2f;
+
+void Start () {
+	colBack=HealthSystem.Colors(0,0,0);//Black color bar
+	colFront=HealthSystem.Colors(0,255,0);//Green color bar - Health indicator
+}
+
+void OnGUI(){
+	HealthSystem.Set_HealthBar(
+		transform,
+		HealthBarHeight,
+		Health,
+		MaxHealth,
+		colBack,
+		colFront);
+}
+
+
+}
