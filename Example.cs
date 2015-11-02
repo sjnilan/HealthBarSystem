dc14e7ae02da2c3cca97a7cb26a19a75fb93b62d
One.Zero <sjnilanr@gmail.com>
2015-11-02 20:44:26 +0530

    Update Example.cs

--- Example.cs
+++ Example.cs
@@ -7,15 +7,16 @@ private Texture2D colBack;
 private Texture2D colFront;
 public float Health=90f;
 public float MaxHealth=100f;
-public float HealthBarHeight=2f;
+public float HealthBarHeight=2;
+HealthSystem  HS =New HealthSystem ();
 
 void Start () {
-	colBack=HealthSystem.Colors(0,0,0);//Black color bar
-	colFront=HealthSystem.Colors(0,255,0);//Green color bar - Health indicator
+	colBack=HS.Colors(0,0,0);//Black color bar
+	colFront=HS.Colors(0,255,0);//Green color bar - Health indicator
 }
 
 void OnGUI(){
-	HealthSystem.Set_HealthBar(
+	HS.Set_HealthBar(
 		transform,
 		HealthBarHeight,
 		Health,
